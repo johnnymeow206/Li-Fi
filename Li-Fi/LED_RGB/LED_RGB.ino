03bd53603432aed3a5ece3d812f1d79053529582
elvisting <74713547+elvisting@users.noreply.github.com>
2022-07-06 13:47:51 +0800

    Add files via upload

---
+++ Li-Fi/LED_RGB/LED_RGB.ino
@@ -0,0 +1,52 @@
+int redPin=3;
+int greenPin=4;
+int bluePin=5;
+
+int redPin1=10;
+int greenPin1=9;
+int bluePin1=8;
+
+
+void setup() {
+  // put your setup code here, to run once:
+Serial.begin(9600);
+pinMode(redPin,OUTPUT);
+pinMode(greenPin,OUTPUT);
+pinMode(bluePin,OUTPUT);
+}
+ 
+void loop() {
+
+  // put your main code here, to run repeatedly:
+
+//red
+  analogWrite(redPin,255);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,0);
+//green
+  analogWrite(redPin1,0);
+  analogWrite(greenPin1,255);
+  analogWrite(bluePin1,0);
+//blue
+/*  analogWrite(redPin,0);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,255);
+//magenta
+  analogWrite(redPin,255);
+  analogWrite(greenPin,0);
+  analogWrite(bluePin,255);
+//yellow
+  analogWrite(redPin,255);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,0);
+//cyan
+  analogWrite(redPin,0);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,255);
+//white
+  analogWrite(redPin,255);
+  analogWrite(greenPin,255);
+  analogWrite(bluePin,255);
+*/
+
+}
