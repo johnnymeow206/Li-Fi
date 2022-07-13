0ba6133f226dc3561b7d876bc4ce5eb2f682b41e
Williamochi <100845264+Williamochi@users.noreply.github.com>
2022-07-13 17:10:43 +0800

    Add files via upload

--- JPEG/new/test.py
+++ JPEG/new/test.py
@@ -4,11 +4,18 @@ from PIL import Image
 import numpy as np
 from sympy import cos, pi
 import FDCT
-
-img = np.zeros((500,500,3), dtype='uint8')   # 快速產生 500x500，每個項目為 [0,0,0] 的三維陣列
-img[150:350, 150:350] = [0,0,255]  # 將中間 200x200 的每個項目內容，改為 [0,0,255]
-cv2.imwrite('oxxostudio.jpg', img)       # 存成 jpg
-cv2.imshow('oxxostudio', img)
+from scipy.fftpack import dct, idct
+def rgb_to_y(img_array):
+    B,G,R = img_array[0], img_array[1], img_array[2]
+    return 0.299 * R + 0.587 * G + 0.114 * B
+img = np.zeros((8,8,3), dtype='uint8')   # 快速產生 500x500，每個項目為 [0,0,0] 的三維陣列
+img[:8,:8] = [255,255,255]  # 將中間 200x200 的每個項目內容，改為 [255,255,255]
+img = np.transpose(img) #讓陣列由列變成行?
+imgy = rgb_to_y(img)
+print(imgy)
+imgy2 = FDCT.FDCT_for_gray(imgy)
+print(imgy2)
+print(FDCT.iFDCT_for_gray(imgy2))
 '''
 rows=256
 cols=256
