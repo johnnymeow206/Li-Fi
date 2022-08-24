13614afc28229196affba6917b8f92194958a466
cactuar3101 <100845308+cactuar3101@users.noreply.github.com>
2022-08-24 15:13:41 +0800

    Update main.py

--- JPEG/main.py
+++ JPEG/main.py
@@ -3,6 +3,8 @@ from sympy import pi
 import cv2
 import Def
 import Def2
+import time
+
 rows=256
 cols=256
 channels =1
@@ -15,109 +17,30 @@ count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
         31]
 
 
-
 img=np.fromfile(r'lena', dtype='uint8')
 img=img.reshape(rows, cols, channels)
 
-img = np.transpose(img) 
-img1 = np.split(img, w, axis=1)
+start = time.time()
 
-img3 = [[[[0 for k1 in range(w)] for k2 in range(w)] for k3 in range(w)] for k4 in range(w)]
-temp_list1 = [[[0 for k1 in range(w)] for k2 in range(w)] for k3 in range(w)]
-
-for i in range(w):
-    img2 = np.dsplit(img1[i], w)
-
-    for j in range(w):
-        img3[i][j] = img2[j]
-#img3 = resized_image.astype(np.float32)
-
-temp02 = 0
-for i in range(w):
-    for j in range(w):
-        img4 = np.asmatrix(img3[i][j])
-        img4 = img4.astype(np.float32)
-        img4 -= 128*np.ones((8,8))
-        img4 = cv2.dct(img4)
-
-        img3[i][j] = img4
-        img3[i][j] = Def.quan(img3[i][j])
-
-        for k in range(8):
-            for l in range(8):
-                img3[i][j][k][l] = round(img3[i][j][k][l])
-
-        img3[i][j] = Def.zigzag(img3[i][j])
-
-        temp = img3[i][j][0] - temp02
-        temp02 = img3[i][j][0]
-        img3[i][j][0] = temp
-        
-        img3[i][j] = Def.RLE_AC(img3[i][j])
-        
-        temp_list1[i][j] = img3[i][j]
-        
-        
-        #print(img3[i][j])
-        
-########Huff+combin
-for i in range(w):
-    for j in range(w):
-        img3[i][j] = Def2.Huff(img3[i][j])
-    img3[i] = ''.join(img3[i])
-img3 = ''.join(img3)
+abc = Def2.encode(img, w)
+#print(abc)
+bcd = Def2.decode(abc, w)
+end = time.time()
+print(format(end-start))
 
 
-output1 = Def2.InvHuff(img3)
-output1 = np.reshape(output1,[32,32])
-#######invDIFF(假設已排序好)    #有warning
-w = 32
-temp02 = 0
-for i in range(w):
-    for j in range(w):
-        temp01 = output1[i][j][0] + temp02
-        output1[i][j][0] = temp01
-        temp02 = output1[i][j][0]
-        #Def.error_check(temp_list1[i][j], output1[i][j])
-'''
-####inv RLE
-for i in range(w):
-    for j in range(w):
-        ###################################<<invRLE>>##################################
-        img3[i][j] = Def.InvRLE_AC(img3[i][j])
-        
-        
-        ################################<<iZig-Zag>>#################################
-        img3[i][j] = Def.izigzag(img3[i][j])
-        
-        ##############################<<IQuantization>>###############################
-        img5 = img3[i][j]
-        img5 = Def.iquan(img5)
-        
-        ###################################<<IDCT>>###################################
-        img4 = np.asmatrix(img5)
-        img4 = img4.astype(np.float32)
-        #img4 = cv2.idct(img4)
-        #img4 = FDCT.iFDCT_for_gray(img4)
-        #img4 = Def.InvFDCT(img4)
-        img4 = Def.iFDCT(img4)
-        img4 += 128*np.ones((8,8))
-        img3[i][j] = img4
-        img3[i][j] = np.clip(img3[i][j],0,255)
-
-g = 32
-img_merge = [[0 for k1 in range(8)] for k2 in range(32)]
-for c in range(g):
-    img_merge[c] = img3[c][0]
+img_merge = [[0 for k1 in range(8)] for k2 in range(w)]
+for c in range(w):
+    img_merge[c] = bcd[c][0]
     b = 1
-    for a in range(31):
-        img_merge[c] = np.hstack((img_merge[c],img3[c][b]))
+    for a in range(w-1):
+        img_merge[c] = np.hstack((img_merge[c],bcd[c][b]))
         b+=1
 
 img_merge1 = img_merge[0]
 e = 0
 f = 1
-for d in range(31):
+for d in range(w-1):
     img_merge1 = np.vstack((img_merge1,img_merge[f]))
     e+=1 
     f+=1
@@ -125,10 +48,9 @@ img = np.transpose(img)
 img_merge1 = np.transpose(img_merge1)
 img_merge1 = img_merge1.astype(np.uint8)
 img_merge1=img_merge1.reshape(rows, cols, channels)
-
 cv2.imshow('temp', img_merge1)
 cv2.waitKey(0) 
-'''
+
 '''
 ########snake diff
 
