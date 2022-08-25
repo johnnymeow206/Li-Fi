a061b4cd76dea62b3eb5cabbcdedb565eaaec5fe
Williamochi <100845264+Williamochi@users.noreply.github.com>
2022-08-25 17:08:31 +0800

    Add files via upload

--- JPEG/new/hamming_try.py
+++ JPEG/new/hamming_try.py
@@ -16,14 +16,14 @@ while len_of_m != len_of_m1:
         p0 += 1
         p1 += 1
     if m1[0][1] == '1':
-        p0 += 1
+        p1 += 1
         p2 += 1
     if m1[0][2] == '1':
+        p0 += 1
         p1 += 1
         p2 += 1
     if m1[0][3] == '1':
         p0 += 1
-        p1 += 1
         p2 += 1
 
     p0_num_one = p0 % 2
@@ -48,11 +48,72 @@ while len_of_m != len_of_m1:
     p0 = p1 = p2 = 0
     d += 4
 m2 = ''.join(m2)
-print(m2)
+#print(m2)
 #  !!要補個偵測最後少於四個的標誌能讓decoder知道!!
 ###############decoder#################
-hamming_743code = ['0000000','0001111','0010011','0011100','0100101','0101010','0110110','0111001',
-                   '1000110','1001001','1010101','1011010','1100011','1101100','1110000','1111111']
+hamming_743code = ['0000000','0001101','0010111','0011010','0100011','0101110','0110100','0111001',
+                   '1000110','1001011','1010001','1011100','1100101','1101000','1110010','1111111']
+def hamming_check(inp1):
+    list001 = inp1
+    for ff01 in range(16):
+        if list001 == hamming_743code[ff01]:
+            return list001[0:4]    #取前四個
+def code_change(inp, i):
+    temp = list(inp)
+    if temp[i] == '0':
+        temp[i] = temp[i].replace('0', '1')
+    else:
+        temp[i] = temp[i].replace('1', '0')
+    temp = ''.join(temp)
+    return temp
+def errorcheck(inp):
+    temp = inp
+    synd_code0 = int(temp[4]) + int(temp[0]) + int(temp[2]) + int(temp[3])
+    synd_code1 = int(temp[5]) + int(temp[0]) + int(temp[1]) + int(temp[2])
+    synd_code2 = int(temp[6]) + int(temp[1]) + int(temp[2]) + int(temp[3])
+    synd_code0 = synd_code0 % 2
+    synd_code1 = synd_code1 % 2
+    synd_code2 = synd_code2 % 2
+    if  synd_code0 == 0 and synd_code1 == 0 and synd_code2 == 0:#沒有錯
+        temp = temp
+    elif synd_code0 == 1 and synd_code1 == 1 and synd_code2 == 0:#第1個錯
+        temp = code_change(temp, 0)
+    elif synd_code0 == 0 and synd_code1 == 1 and synd_code2 == 1:#第2個錯
+        temp = code_change(temp, 1)
+    elif synd_code0 == 1 and synd_code1 == 1 and synd_code2 == 1:#第3個錯
+        temp = code_change(temp, 2)
+    elif synd_code0 == 1 and synd_code1 == 0 and synd_code2 == 1:#第4個錯
+        temp = code_change(temp, 3)
+    elif synd_code0 == 1 and synd_code1 == 0 and synd_code2 == 0:#第5個錯
+        temp = code_change(temp, 4)
+    elif synd_code0 == 0 and synd_code1 == 1 and synd_code2 == 0:#第6個錯
+        temp = code_change(temp, 5)
+    elif synd_code0 == 0 and synd_code1 == 0 and synd_code2 == 1:#第7個錯
+        temp = code_change(temp, 6)
+    return temp
+'''
+m3 = '1011010'
+#TEMP00 = code_change(m3, 0)
+TEMP00 = errorcheck(m3)
+print(TEMP00)
+'''
+m3 = '10110100111001'#假設第一位錯
+decode_m1 = m3
+decode_m2 = ['']
+now = 0
+len_m2 = 0.
+block_decode_m1 = len(decode_m1)/7
+block_decode_m2 = len_m2/4
+while block_decode_m1 != block_decode_m2:
+    TEMP00 = decode_m1[now:now+7]
+    TEMP00 = errorcheck(TEMP00)
+    decode_m2.append(hamming_check(TEMP00))
+    decode_m2 = [''.join(decode_m2)]
+    len_m2 = len(decode_m2[0])
+    block_decode_m2 = len_m2/4
+    now += 7
+print(decode_m2)
+'''
 de_m = m2
 block_de_m = len(de_m)/7
 temp_m = []
@@ -85,6 +146,11 @@ while block_de_m != block_de_m1:
     de_m1.append(temp_m1)
     de_m1 = [''.join(de_m1)]
     print(de_m1)
+
+
+
+
+'''
 
 
 
--- JPEG/new/main.py
+++ JPEG/new/main.py
@@ -3,6 +3,8 @@ from sympy import pi
 import cv2
 import Def
 import Def2
+import time
+
 rows=256
 cols=256
 channels =1
@@ -18,95 +20,27 @@ count = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
 img=np.fromfile(r'lena', dtype='uint8')
 img=img.reshape(rows, cols, channels)
 
-img = np.transpose(img) 
-img1 = np.split(img, w, axis=1)
+start = time.time()
 
-img3 = [[[[0 for k1 in range(w)] for k2 in range(w)] for k3 in range(w)] for k4 in range(w)]
-
-for i in range(w):
-    img2 = np.dsplit(img1[i], w)
-
-    for j in range(w):
-        img3[i][j] = img2[j]
-#img3 = resized_image.astype(np.float32)
+abc = Def2.encode(img, w)
+#print(abc)
+bcd = Def2.decode(abc, w)
+end = time.time()
+print(format(end-start))
 
 
-for i in range(w):
-    for j in range(w):
-        img4 = np.asmatrix(img3[i][j])
-        img4 = img4.astype(np.float32)
-        img4 -= 128*np.ones((8,8))
-        #img4 = cv2.dct(img4)
-        img4 = Def.FDCT3(img4)
-        img3[i][j] = img4
-        img3[i][j] = Def.quan(img3[i][j])
-        img3[i][j] = np.round_(img3[i][j],0)
-        '''
-        for k in range(8):
-            for l in range(8):
-                img3[i][j][k][l] = round(img3[i][j][k][l])
-        '''
-        img3[i][j] = Def.zigzag(img3[i][j])
-        img3[i][j] = Def.RLE_AC(img3[i][j])
-        #print(img3[i][j])
-
-########DIFF
-temp02 = 0
-for i in range(w):
-    for j in range(w):
-        temp = img3[j][i][0] - temp02
-        temp02 = img3[j][i][0]
-        img3[j][i][0] = temp
-########Huff+combin
-for i in range(w):
-    for j in range(w):
-        img3[i][j] = Def2.Huff(img3[i][j])
-    img3[i] = ''.join(img3[i])
-img3 = ''.join(img3)
-
-
-
-img3 = Def2.InvHuff(img3)
-img3 = np.reshape(img3,[32,32])
-#######invDIFF(假設已排序好)    #有warning
-img3 = Def.invDiff(img3)
-####inv RLE
-for i in range(w):
-    for j in range(w):
-        ###################################<<invRLE>>##################################
-        img3[i][j] = Def.InvRLE_AC(img3[i][j])
-        
-        ################################<<iZig-Zag>>#################################
-        img3[i][j] = Def.izigzag(img3[i][j])
-        
-        ##############################<<IQuantization>>###############################
-        img5 = img3[i][j]
-        img5 = Def.iquan(img5)
-        
-        ###################################<<IDCT>>###################################
-        img4 = np.asmatrix(img5)
-        img4 = img4.astype(np.float32)
-        #img4 = cv2.idct(img4)
-        #img4 = FDCT.iFDCT_for_gray(img4)
-        #img4 = Def.InvFDCT(img4)
-        img4 = Def.iFDCT3(img4)
-        img4 += 128*np.ones((8,8))
-        img3[i][j] = img4
-        img3[i][j] = np.clip(img3[i][j],0,255)
-
-g = 32
-img_merge = [[0 for k1 in range(8)] for k2 in range(32)]
-for c in range(g):
-    img_merge[c] = img3[c][0]
+img_merge = [[0 for k1 in range(8)] for k2 in range(w)]
+for c in range(w):
+    img_merge[c] = bcd[c][0]
     b = 1
-    for a in range(31):
-        img_merge[c] = np.hstack((img_merge[c],img3[c][b]))
+    for a in range(w-1):
+        img_merge[c] = np.hstack((img_merge[c],bcd[c][b]))
         b+=1
 
 img_merge1 = img_merge[0]
 e = 0
 f = 1
-for d in range(31):
+for d in range(w-1):
     img_merge1 = np.vstack((img_merge1,img_merge[f]))
     e+=1 
     f+=1
@@ -114,9 +48,9 @@ img = np.transpose(img)
 img_merge1 = np.transpose(img_merge1)
 img_merge1 = img_merge1.astype(np.uint8)
 img_merge1=img_merge1.reshape(rows, cols, channels)
-
 cv2.imshow('temp', img_merge1)
 cv2.waitKey(0) 
+
 '''
 ########snake diff
 
